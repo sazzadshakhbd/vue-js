77a3820fc53538fc43242a708aa622ada43b513b
sazzadshakhbd <sazzadshakhbd@gmail.com>
2020-01-08 00:24:39 +0600

    create alert component

--- Youtube/Code-Inspire/basic-vue/src/main.js
+++ Youtube/Code-Inspire/basic-vue/src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+Vue.component("alert", require('./components/Alert.vue').default);
+
 new Vue({
   router,
   render: h => h(App)
