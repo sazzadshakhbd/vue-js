2b15b13299b958880fc00f70812111eb19988f2c
sazzadshakhbd <sazzadshakhbd@gmail.com>
2020-01-16 10:10:45 +0600

    create basic todo apps

--- Youtube/Code-Inspire/basic-vue/src/components/TodoList.vue
+++ Youtube/Code-Inspire/basic-vue/src/components/TodoList.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="todolist">
     <h1>Todo List Apps</h1>
+    <input @keyup.enter="add" type="text" v-model="title" placeholder="What you want to do?">
+    <button @click="add">Add to list</button>
+    <ul>
+      <li v-for="x in todos">{{x}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TodoList"
+  name: "TodoList",
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+      title: '',
+      todos: [
+        'Go to shop'
+      ]
+    }
+  },
+  methods: {
+    add(){
+      this.todos.push(this.title)
+      this.title='';
+    }
+  }
 };
 </script>
 
