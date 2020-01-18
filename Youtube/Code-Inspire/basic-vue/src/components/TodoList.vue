964a6244ac10c576c1cbdbbbe49ea12fd1680965
sazzadshakhbd <sazzadshakhbd@gmail.com>
2020-01-18 12:18:27 +0600

    how to dellete list in forlopp using todo apps

--- Youtube/Code-Inspire/basic-vue/src/components/TodoList.vue
+++ Youtube/Code-Inspire/basic-vue/src/components/TodoList.vue
@@ -4,7 +4,9 @@
     <input @keyup.enter="add" type="text" v-model="title" placeholder="What you want to do?">
     <button @click="add">Add to list</button>
     <ul>
-      <li v-for="x in todos">{{x}}</li>
+      <li v-for="(x, index) in todos">{{x}}
+        <span @click="remove(index)" class="remove">x</span>
+      </li>
     </ul>
   </div>
 </template>
@@ -25,13 +27,28 @@ export default {
   },
   methods: {
     add(){
-      this.todos.push(this.title)
-      this.title='';
+      if(this.title != ''){
+        this.todos.unshift(this.title)
+        this.title='';
+      }
+    },
+    remove(id) {
+      // console.log(id);
+      // this.todos.splice(id,1)
+      this.$delete(this.todos, id);
     }
   }
 };
 </script>
 
 <style scoped lang="scss">
-
+  .remove {
+    padding: 5px 7px;
+    cursor: pointer;
+    display: inline-block;
+    margin-bottom: 5px;
+    background: teal;
+    color: #fff;
+    border-radius: 10px;
+  }
 </style>
