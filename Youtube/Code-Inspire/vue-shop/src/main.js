8f2dd84b763d9b4d6f86c9adb9b89d2257c93a20
sazzadshakhbd <sazzadshakhbd@gmail.com>
2020-01-19 10:19:26 +0600

    create a ecommerce project and setup bootstrap

---
+++ Youtube/Code-Inspire/vue-shop/src/main.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import 'bootstrap';
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+import 'popper.js';
+import './assets/app.scss';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
