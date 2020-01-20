f1f575c31e46300ef3d8600e97b3902f1937ac07
sazzadshakhbd <sazzadshakhbd@gmail.com>
2020-01-20 09:42:54 +0600

    create admin template and create component for index template

--- Youtube/Code-Inspire/vue-shop/src/App.vue
+++ Youtube/Code-Inspire/vue-shop/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <NavBar></NavBar>
     <router-view />
   </div>
 </template>
--- Youtube/Code-Inspire/vue-shop/src/views/Home.vue
+++ Youtube/Code-Inspire/vue-shop/src/views/Home.vue
@@ -1,15 +1,35 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <NavBar></NavBar>
+    <Hero></Hero>
+    <Products></Products>
+    <div class="container h-100">
+      <div class="row p-5 h-100 justify-content-center align-items-center">
+        <div class="col-md-6">
+          <img src="/img/svg/product.svg" alt width="400" />
+        </div>
+        <div class="col-md-6">
+          <h3>Learn More About us</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quaerat voluptate iusto? Nihil voluptas vitae veritatis magnam reprehenderit, reiciendis ex? Libero quasi natus veniam voluptatum, voluptas exercitationem ratione consectetur et!</p>
+          <button class="btn custom-btn btn-outline-primary mr-4">Browse Products</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import NavBar from "@/components/NavBar.vue";
+import Hero from "@/components/Hero.vue";
+import Products from "@/sections/Products.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    NavBar,
+    Hero,
+    Products
   }
 };
 </script>
