a6f6b3c314466b26e89ec2df57bd4294903f88c8
sazzadshakhbd <sazzadshakhbd@gmail.com>
2020-01-21 09:41:31 +0600

    creae admin template and linking toogle class for sidebar

--- Youtube/Code-Inspire/vue-shop/src/views/Admin.vue
+++ Youtube/Code-Inspire/vue-shop/src/views/Admin.vue
@@ -66,89 +66,6 @@
                   </ul>
                 </div>
               </li>
-              <li class="sidebar-dropdown">
-                <a href="#">
-                  <i class="fa fa-shopping-cart"></i>
-                  <span class="menu-text">E-commerce</span>
-                  <span class="badge badge-pill badge-danger">3</span>
-                </a>
-                <div class="sidebar-submenu">
-                  <ul>
-                    <li>
-                      <a href="#">Products</a>
-                    </li>
-                    <li>
-                      <a href="#">Orders</a>
-                    </li>
-                    <li>
-                      <a href="#">Credit cart</a>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-              <li class="sidebar-dropdown">
-                <a href="#">
-                  <i class="far fa-gem"></i>
-                  <span class="menu-text">Components</span>
-                </a>
-                <div class="sidebar-submenu">
-                  <ul>
-                    <li>
-                      <a href="#">General</a>
-                    </li>
-                    <li>
-                      <a href="#">Panels</a>
-                    </li>
-                    <li>
-                      <a href="#">Tables</a>
-                    </li>
-                    <li>
-                      <a href="#">Icons</a>
-                    </li>
-                    <li>
-                      <a href="#">Forms</a>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-              <li class="sidebar-dropdown">
-                <a href="#">
-                  <i class="fa fa-chart-line"></i>
-                  <span class="menu-text">Charts</span>
-                </a>
-                <div class="sidebar-submenu">
-                  <ul>
-                    <li>
-                      <a href="#">Pie chart</a>
-                    </li>
-                    <li>
-                      <a href="#">Line chart</a>
-                    </li>
-                    <li>
-                      <a href="#">Bar chart</a>
-                    </li>
-                    <li>
-                      <a href="#">Histogram</a>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-              <li class="sidebar-dropdown">
-                <a href="#">
-                  <i class="fa fa-globe"></i>
-                  <span class="menu-text">Maps</span>
-                </a>
-                <div class="sidebar-submenu">
-                  <ul>
-                    <li>
-                      <a href="#">Google maps</a>
-                    </li>
-                    <li>
-                      <a href="#">Open street map</a>
-                    </li>
-                  </ul>
-                </div>
-              </li>
               <li class="header-menu">
                 <span>Extra</span>
               </li>
@@ -175,150 +92,6 @@
           </div>
           <!-- sidebar-menu  -->
         </div>
-        <!-- sidebar-footer  -->
-        <div class="sidebar-footer">
-          <div class="dropdown">
-            <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fa fa-bell"></i>
-              <span class="badge badge-pill badge-warning notification">3</span>
-            </a>
-            <div class="dropdown-menu notifications" aria-labelledby="dropdownMenuMessage">
-              <div class="notifications-header">
-                <i class="fa fa-bell"></i>
-                Notifications
-              </div>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">
-                <div class="notification-content">
-                  <div class="icon">
-                    <i class="fas fa-check text-success border border-success"></i>
-                  </div>
-                  <div class="content">
-                    <div class="notification-detail">
-                      Lorem ipsum dolor sit amet consectetur adipisicing
-                      elit. In totam explicabo
-                    </div>
-                    <div class="notification-time">6 minutes ago</div>
-                  </div>
-                </div>
-              </a>
-              <a class="dropdown-item" href="#">
-                <div class="notification-content">
-                  <div class="icon">
-                    <i class="fas fa-exclamation text-info border border-info"></i>
-                  </div>
-                  <div class="content">
-                    <div class="notification-detail">
-                      Lorem ipsum dolor sit amet consectetur adipisicing
-                      elit. In totam explicabo
-                    </div>
-                    <div class="notification-time">Today</div>
-                  </div>
-                </div>
-              </a>
-              <a class="dropdown-item" href="#">
-                <div class="notification-content">
-                  <div class="icon">
-                    <i class="fas fa-exclamation-triangle text-warning border border-warning"></i>
-                  </div>
-                  <div class="content">
-                    <div class="notification-detail">
-                      Lorem ipsum dolor sit amet consectetur adipisicing
-                      elit. In totam explicabo
-                    </div>
-                    <div class="notification-time">Yesterday</div>
-                  </div>
-                </div>
-              </a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item text-center" href="#">View all notifications</a>
-            </div>
-          </div>
-          <div class="dropdown">
-            <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fa fa-envelope"></i>
-              <span class="badge badge-pill badge-success notification">7</span>
-            </a>
-            <div class="dropdown-menu messages" aria-labelledby="dropdownMenuMessage">
-              <div class="messages-header">
-                <i class="fa fa-envelope"></i>
-                Messages
-              </div>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">
-                <div class="message-content">
-                  <div class="pic">
-                    <img src="img/user.jpg" alt />
-                  </div>
-                  <div class="content">
-                    <div class="message-title">
-                      <strong>Jhon doe</strong>
-                    </div>
-                    <div class="message-detail">
-                      Lorem ipsum dolor sit amet consectetur adipisicing
-                      elit. In totam explicabo
-                    </div>
-                  </div>
-                </div>
-              </a>
-              <a class="dropdown-item" href="#">
-                <div class="message-content">
-                  <div class="pic">
-                    <img src="img/user.jpg" alt />
-                  </div>
-                  <div class="content">
-                    <div class="message-title">
-                      <strong>Jhon doe</strong>
-                    </div>
-                    <div class="message-detail">
-                      Lorem ipsum dolor sit amet consectetur adipisicing
-                      elit. In totam explicabo
-                    </div>
-                  </div>
-                </div>
-              </a>
-              <a class="dropdown-item" href="#">
-                <div class="message-content">
-                  <div class="pic">
-                    <img src="img/user.jpg" alt />
-                  </div>
-                  <div class="content">
-                    <div class="message-title">
-                      <strong>Jhon doe</strong>
-                    </div>
-                    <div class="message-detail">
-                      Lorem ipsum dolor sit amet consectetur adipisicing
-                      elit. In totam explicabo
-                    </div>
-                  </div>
-                </div>
-              </a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item text-center" href="#">View all messages</a>
-            </div>
-          </div>
-          <div class="dropdown">
-            <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fa fa-cog"></i>
-              <span class="badge-sonar"></span>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuMessage">
-              <a class="dropdown-item" href="#">My profile</a>
-              <a class="dropdown-item" href="#">Help</a>
-              <a class="dropdown-item" href="#">Setting</a>
-            </div>
-          </div>
-          <div>
-            <a href="#">
-              <i class="fa fa-power-off"></i>
-            </a>
-          </div>
-          <div class="pinned-footer">
-            <a href="#">
-              <i class="fas fa-ellipsis-h"></i>
-            </a>
-          </div>
-        </div>
       </nav>
       <!-- page-content  -->
       <main class="page-content pt-2">
@@ -329,112 +102,23 @@
               <h2>Pro Sidebar</h2>
               <p>This is a responsive sidebar template with dropdown menu based on bootstrap framework.</p>
             </div>
-            <div class="form-group col-md-12">
-              <iframe
-                src="https://ghbtns.com/github-btn.html?user=azouaoui-med&repo=pro-sidebar-template&type=star&count=true&size=small"
-                frameborder="0"
-                scrolling="0"
-                width="100px"
-                height="30px"
-              ></iframe>
-              <iframe
-                src="https://ghbtns.com/github-btn.html?user=azouaoui-med&repo=pro-sidebar-template&type=fork&count=true&size=small"
-                frameborder="0"
-                scrolling="0"
-                width="100px"
-                height="30px"
-              ></iframe>
-            </div>
 
             <div class="form-group col-md-12">
-              <a id="toggle-sidebar" class="btn btn-secondary rounded-0" href="#">
+              <a
+                id="toggle-sidebar"
+                @click="closeMenu"
+                class="btn btn-secondary rounded-0"
+                href="#"
+              >
                 <span>Toggle Sidebar</span>
               </a>
-              <a id="pin-sidebar" class="btn btn-outline-secondary rounded-0" href="#">
+              <a
+                id="pin-sidebar"
+                @click="closeMenu"
+                class="btn btn-outline-secondary rounded-0"
+                href="#"
+              >
                 <span>Pin Sidebar</span>
-              </a>
-            </div>
-          </div>
-          <hr />
-          <div class="row">
-            <div class="form-group col-md-12">
-              <h3>Themes</h3>
-              <p>Here are more themes that you can use</p>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="form-group col-md-12">
-              <a href="#" data-theme="default-theme" class="theme default-theme selected"></a>
-              <a href="#" data-theme="chiller-theme" class="theme chiller-theme"></a>
-              <a href="#" data-theme="legacy-theme" class="theme legacy-theme"></a>
-              <a href="#" data-theme="ice-theme" class="theme ice-theme"></a>
-              <a href="#" data-theme="cool-theme" class="theme cool-theme"></a>
-              <a href="#" data-theme="light-theme" class="theme light-theme"></a>
-            </div>
-            <div class="form-group col-md-12">
-              <p>You can also use background image</p>
-            </div>
-            <div class="form-group col-md-12">
-              <a href="#" data-bg="bg1" class="theme theme-bg selected"></a>
-              <a href="#" data-bg="bg2" class="theme theme-bg"></a>
-              <a href="#" data-bg="bg3" class="theme theme-bg"></a>
-              <a href="#" data-bg="bg4" class="theme theme-bg"></a>
-            </div>
-            <div class="form-group col-md-12">
-              <div class="custom-control custom-switch">
-                <input type="checkbox" class="custom-control-input" id="toggle-bg" checked />
-                <label class="custom-control-label" for="toggle-bg">Background image</label>
-              </div>
-            </div>
-            <div class="form-group col-md-12">
-              <div class="custom-control custom-switch">
-                <input type="checkbox" class="custom-control-input" id="toggle-border-radius" />
-                <label class="custom-control-label" for="toggle-border-radius">Border radius</label>
-              </div>
-            </div>
-          </div>
-
-          <hr />
-          <div class="row">
-            <div class="form-group col-md-12">
-              <small>
-                Made with
-                <i class="fa fa-heart text-danger" aria-hidden="true"></i> by
-                <span class="text-secondary font-weight-bold">
-                  Mohamed
-                  Azouaoui
-                </span>
-              </small>
-            </div>
-            <div class="form-group col-md-12">
-              <a
-                href="https://github.com/azouaoui-med"
-                target="_blank"
-                class="btn btn-sm bg-secondary shadow-sm rounded-0 text-light mr-3 mb-3"
-              >
-                <i class="fab fa-github" aria-hidden="true"></i>
-              </a>
-              <a
-                href="https://twitter.com/azouaoui_med"
-                target="_blank"
-                class="btn btn-sm bg-secondary shadow-sm rounded-0 text-light mr-3 mb-3"
-              >
-                <i class="fab fa-twitter" aria-hidden="true"></i>
-              </a>
-              <a
-                href="https://www.instagram.com/azouaoui_med/"
-                target="_blank"
-                class="btn btn-sm bg-secondary shadow-sm rounded-0 text-light mr-3 mb-3"
-              >
-                <i class="fab fa-instagram" aria-hidden="true"></i>
-              </a>
-              <a
-                href="https://www.linkedin.com/in/mohamed-azouaoui/"
-                target="_blank"
-                class="btn btn-sm bg-secondary shadow-sm rounded-0 text-light mr-3 mb-3"
-              >
-                <i class="fab fa-linkedin-in" aria-hidden="true"></i>
               </a>
             </div>
           </div>
@@ -446,11 +130,12 @@
   </div>
 </template>
 <script>
-import Hero from "@/components/Hero.vue";
 export default {
   name: "Admin",
-  components: {
-    Hero
+  methods: {
+    closeMenu() {
+      $(".page-wrapper").toggleClass("toggled");
+    }
   }
 };
 </script>
