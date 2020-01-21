9ba19ab0d3eebf581bb28dd0e321bddc2d76067e
sazzadshakhbd <sazzadshakhbd@gmail.com>
2020-01-21 12:03:43 +0600

    admin component redesign

--- Youtube/Code-Inspire/vue-shop/src/views/Admin.vue
+++ Youtube/Code-Inspire/vue-shop/src/views/Admin.vue
@@ -1,143 +1,125 @@
 <template>
-  <div class="admin">
-    <div class="page-wrapper default-theme sidebar-bg bg1 toggled">
-      <nav id="sidebar" class="sidebar-wrapper">
-        <div class="sidebar-content">
-          <!-- sidebar-brand  -->
-          <div class="sidebar-item sidebar-brand">
-            <a href="#">pro sidebar</a>
+<div class="admin">
+  <div class="page-wrapper default-theme sidebar-bg bg1 toggled">
+    <a id="show-sidebar" @click="closeMenu" class="btn btn-sm btn-dark" href="#" style="position: absolute;left: 5px;top: 5px;">
+      <i class="fas fa-bars"></i>
+    </a>
+    <nav id="sidebar" class="sidebar-wrapper">
+      <div class="sidebar-content">
+        <!-- sidebar-brand  -->
+        <div class="sidebar-item sidebar-brand justify-content-between">
+          <div>
+            <a href="#">Vue Shop</a>
           </div>
-          <!-- sidebar-header  -->
-          <div class="sidebar-item sidebar-header d-flex flex-nowrap">
-            <div class="user-pic">
-              <img class="img-responsive img-rounded" src="img/user.jpg" alt="User picture" />
-            </div>
-            <div class="user-info">
-              <span class="user-name">
-                Jhon
-                <strong>Smith</strong>
-              </span>
-              <span class="user-role">Administrator</span>
-              <span class="user-status">
-                <i class="fa fa-circle"></i>
-                <span>Online</span>
-              </span>
-            </div>
+          <div id="close-sidebar" @click="closeMenu" style="cursor:pointer;">
+            <i class="fas fa-times"></i>
           </div>
-          <!-- sidebar-search  -->
-          <div class="sidebar-item sidebar-search">
-            <div>
-              <div class="input-group">
-                <input type="text" class="form-control search-menu" placeholder="Search..." />
-                <div class="input-group-append">
-                  <span class="input-group-text">
-                    <i class="fa fa-search" aria-hidden="true"></i>
-                  </span>
-                </div>
+        </div>
+        <!-- sidebar-header  -->
+        <div class="sidebar-item sidebar-header d-flex align-items-center">
+          <div class="user-pic">
+            <img class="img-responsive img-rounded" src="/img/user.png" alt="User picture">
+          </div>
+          <div class="user-info text-left">
+            <span class="user-name">Jhon
+              <strong>Smith</strong>
+            </span>
+            <span class="user-role"> Aministrator </span>
+            <span class="user-status">
+              <i class="fa fa-circle"></i>
+              <span>Online</span>
+            </span>
+          </div>
+        </div>
+        <!-- sidebar-search  -->
+        <div class="sidebar-item sidebar-search">
+          <div>
+            <div class="input-group">
+              <input type="text" class="form-control search-menu" placeholder="Search...">
+              <div class="input-group-append">
+                <span class="input-group-text">
+                  <i class="fa fa-search" aria-hidden="true"></i>
+                </span>
               </div>
             </div>
           </div>
-          <!-- sidebar-menu  -->
-          <div class="sidebar-item sidebar-menu">
-            <ul>
-              <li class="header-menu">
-                <span>General</span>
-              </li>
-              <li class="sidebar-dropdown">
-                <a href="#">
-                  <i class="fa fa-tachometer-alt"></i>
-                  <span class="menu-text">Dashboard</span>
-                  <span class="badge badge-pill badge-warning">New</span>
-                </a>
-                <div class="sidebar-submenu">
-                  <ul>
-                    <li>
-                      <a href="#">
-                        Dashboard 1
-                        <span class="badge badge-pill badge-success">Pro</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">Dashboard 2</a>
-                    </li>
-                    <li>
-                      <a href="#">Dashboard 3</a>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-              <li class="header-menu">
-                <span>Extra</span>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-book"></i>
-                  <span class="menu-text">Documentation</span>
-                  <span class="badge badge-pill badge-primary">Beta</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-calendar"></i>
-                  <span class="menu-text">Calendar</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-folder"></i>
-                  <span class="menu-text">Examples</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <!-- sidebar-menu  -->
         </div>
-      </nav>
-      <!-- page-content  -->
-      <main class="page-content pt-2">
-        <div id="overlay" class="overlay"></div>
-        <div class="container-fluid p-5">
-          <div class="row">
-            <div class="form-group col-md-12">
-              <h2>Pro Sidebar</h2>
-              <p>This is a responsive sidebar template with dropdown menu based on bootstrap framework.</p>
-            </div>
+        <!-- sidebar-menu  -->
+        <div class=" sidebar-item sidebar-menu">
+          <ul>
+            <li class="header-menu">
+              <span>Menu</span>
+            </li>
 
-            <div class="form-group col-md-12">
-              <a
-                id="toggle-sidebar"
-                @click="closeMenu"
-                class="btn btn-secondary rounded-0"
-                href="#"
-              >
-                <span>Toggle Sidebar</span>
+            <li>
+              <a href="#">
+                <i class="fa fa-chart-line"></i>
+                <span>Overview</span>
               </a>
-              <a
-                id="pin-sidebar"
-                @click="closeMenu"
-                class="btn btn-outline-secondary rounded-0"
-                href="#"
-              >
-                <span>Pin Sidebar</span>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fab fa-amazon"></i>
+                <span>Products</span>
               </a>
-            </div>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-shopping-cart"></i>
+                <span>Orders</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-power-off"></i>
+                <span>Logout</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <!-- sidebar-menu  -->
+      </div>
+
+    </nav>
+    <!-- sidebar-content  -->
+    <main class="page-content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="form-group col-md-12">
+            <h2>Sidebar Tempalte</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi harum consequatur, beatae impedit eius libero placeat facilis dolore neque non voluptatum maiores officia sequi cupiditate voluptates quas fugit nihil nemo.</p>
           </div>
         </div>
-      </main>
-      <!-- page-content" -->
-    </div>
-    <!-- page-wrapper -->
+      </div>
+    </main>
+    <!-- page-content" -->
   </div>
+  <!-- page-wrapper -->
+
+</div>
 </template>
+
 <script>
+
+
 export default {
-  name: "Admin",
+  name: "admin",
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    
+  },
   methods: {
     closeMenu() {
       $(".page-wrapper").toggleClass("toggled");
     }
-  }
+  },
 };
 </script>
-<style lang="scss">
+
+<style>
+
 </style>
