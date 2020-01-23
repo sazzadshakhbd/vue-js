7159f8e80756cc688deba0a621b970946da8d758
sazzadshakhbd <sazzadshakhbd@gmail.com>
2020-01-23 13:52:48 +0600

    firbase install and integration

---
+++ Youtube/Code-Inspire/vue-shop/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBv4MItUPI0uQTlaNK2QOBjiDWkdfVwB9Q",
+  authDomain: "vue-shop-1f69e.firebaseapp.com",
+  databaseURL: "https://vue-shop-1f69e.firebaseio.com",
+  projectId: "vue-shop-1f69e",
+  storageBucket: "vue-shop-1f69e.appspot.com",
+  messagingSenderId: "695943390360",
+  appId: "1:695943390360:web:045acfe2e024fed4441ced",
+  measurementId: "G-CV2MGXPQGP"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
